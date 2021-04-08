8e62c6f38300ac7bb0570d91cd30d03bbd40073e
Johanna <johannatovebergstrom@gmail.com>
2021-04-08 14:49:00 +0200

    Small fixes

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const Sidebar = () =>{
+    return(
+        <div class="menu">
+            <a class="menu__link" href="">SVT 1</a>
+            <a class="menu__link" href="">SVT 2</a>
+            <a class="menu__link" href="">SVT Barn</a>
+            <a class="menu__link" href="">Kunskapskanalen</a>
+            <a class="menu__link" href="">SVT 24</a>        
+        </div>
+    )
+}
+//exporteras som en JavaScript komponent
+export default Sidebar
